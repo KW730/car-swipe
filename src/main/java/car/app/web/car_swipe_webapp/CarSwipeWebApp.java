a7e24accb57c30218db2b917cae80a05b90dfe58
KW730 <krystian.wypart99@gmail.com>
2024-12-09 23:07:50 +0100

    test

---
+++ src/main/java/car/app/web/car_swipe_webapp/CarSwipeWebApp.java
@@ -0,0 +1,13 @@
+package car.app.web.car_swipe_webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarSwipeWebApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarSwipeWebApp.class, args);
+	}
+
+}
